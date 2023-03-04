050adf476dbb0832f04abac3ebfd2c5c1b537399
Vignettese <gwolf204@gmail.com>
2023-03-04 03:30:02 -0500

    comment from python script

--- gitactions.py
+++ gitactions.py
@@ -8,10 +8,10 @@ def git_push():
         repo = Repo(PATH_OF_GIT_REPO)
         repo.git.add(update=True)
         repo.index.commit(COMMIT_MESSAGE)
-        origin = repo.remote(name='Main')
+        origin = repo.remote(name='main')
         origin.push()
     except:
         print('Some error occured while pushing the code')    
 
 git_push() 
-#
+##
